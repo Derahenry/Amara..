befb55b8b91402c62375e27508f0f85fd8c8a3cd
Derahenry <161659149+Derahenry@users.noreply.github.com>
2024-09-25 10:50:50 +0100

    Add files via upload

---
+++ vs code javascript 2/assignment2.js
@@ -0,0 +1,14 @@
+//creating a function 
+function multiply num (num * num * num){
+    let cube = num * num * num 
+    return cube
+}
+
+//calling of function 
+const cube1 = 5
+const cube2 = 15
+const cube3 = 20
+
+console.log (num1)
+console.log (num2)
+console.log (num3)
---
+++ vscode javascriptyassignment/assignment.js
@@ -0,0 +1,14 @@
+//creating a function.
+function multiply (length, breadth){
+let Area_of_rectangle = length* breadth
+    return Area_of_rectangle 
+}
+
+//calling a function 
+const Area1 = (multiply (10, 20))
+const Area2 = (multiply (6, 12))
+const Area3 = (multiply (14, 7))
+
+console.log (Area1)
+console.log (Area2)
+console.log (Area3)
